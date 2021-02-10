476dff32c58206692918c478fb5ad8fd2536c1a2
ShirishaZensar <77830811+ShirishaZensar@users.noreply.github.com>
2021-02-10 08:59:54 +0530

    Add model as .cs file

---
+++ RestaurantApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantWork9Feb.Models
+{
+	public enum Cuisine
+	{
+		None,
+		French,
+		Indian,
+		Mexican
+	}
+
+	public class RestaurantApp
+	{
+		private Cuisine _c;
+		public int RId { get; set; }
+
+		public string RName { get; set; }
+
+		public Cuisine CuisineDish
+		{
+			get { return _c; }
+			set { _c = value; }
+		}
+	}
+}
